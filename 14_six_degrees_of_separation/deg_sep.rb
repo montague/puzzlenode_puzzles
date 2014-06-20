c79ba6bf30d90f5bff609e075e03b700539150e4
ian asaff <ian.asaff@gmail.com>
2014-06-20 18:51:57 -0400

    wip

--- 14_six_degrees_of_separation/deg_sep.rb
+++ 14_six_degrees_of_separation/deg_sep.rb
@@ -1,6 +1,22 @@
+require 'strscan'
+require 'byebug'
 class DegSep
-  def parse_speaker(s)
-    /\A(.*)?:/.match(s)[1]
+  attr_reader :connections
+
+  def initialize
+    @connections = Hash.new{ |h,k| h[k] = [] }
+  end
+
+  def parse(str)
+    scanner = StringScanner.new(str)
+    speaker = scanner.scan_until(/:/).chop
+    names = []
+    loop do
+      break unless scanner.match?(/.*@/)
+      scanner.scan_until(/@/)
+      names << scanner.scan_until(/\W/).chop
+    end
+    @connections[speaker] << names.sort
   end
 end
 
@@ -14,10 +30,22 @@ if $0 == __FILE__
     end
   end
 else
-  describe DegSep, '#parse_speaker' do
-    it "works" do
-      tweet = "alberta: @bob \"It is remarkable, the character of the pleasure we derive from the best books.\""
+  describe DegSep, '#parse' do
+    it "works on simple" do
+      d = DegSep.new
+      s = "alberta: @bob \"It is remarkable, the character of the pleasure we derive from the best books.\""
+      d.parse(s)
 
+      expect(d.connections.keys).to match_array ["alberta"]
+      expect(d.connections['alberta'].first).to match_array ['bob']
+    end
+
+    it 'works on less complicated' do
+      d = DegSep.new
+      s = "daniella_hamill: @madelyn, @concepcion_hoppe: Power is in nature the essential measure  of right"
+      d.parse(s)
+      expect(d.connections.keys).to eq ["daniella_hamill"]
+      expect(d.connections['daniella_hamill'].first).to match_array %w(concepcion_hoppe madelyn)
     end
   end
 end
