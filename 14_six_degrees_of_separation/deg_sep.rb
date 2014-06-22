9a4afb75d680716c791789bceb75319876ef6c5d
ian asaff <ian.asaff@gmail.com>
2014-06-22 12:01:19 -0400

    wip

--- 14_six_degrees_of_separation/deg_sep.rb
+++ 14_six_degrees_of_separation/deg_sep.rb
@@ -7,6 +7,12 @@ class DegSep
     @connections = Hash.new{ |h,k| h[k] = [] }
   end
 
+  def run(input_file, output_file)
+    parse_from_file(input_file)
+    find_all_connections
+    to_file(output_file)
+  end
+
   def parse_from_file(file)
     File.readlines(file).each do |line|
       parse(line)
@@ -17,15 +23,41 @@ class DegSep
   def prune_non_first_order_connections
     @connections.dup.each do |speaker, names|
       names[0].each do |name|
+        next if @connections[name][0].nil?
         unless @connections[name][0].include?(speaker)
           @connections[speaker][0].delete(name)
+          if @connections[speaker][0].empty?
+            @connections[speaker].pop
+          end
         end
       end
     end
   end
 
-  def find_mutual_connections_for(speaker, degree)
+  def find_all_connections
+    @connections.keys.dup.each do |speaker|
+      find_mutual_connections_for(speaker)
+    end
+  end
 
+  def find_mutual_connections_for(speaker, degree=1)
+    return if @connections[speaker].empty?
+    @connections[speaker][degree] ||= []
+    @connections[speaker][degree - 1].each do |name|
+      next if @connections[name][0].nil?
+      @connections[name][0].each do |new_name|
+        next if @connections[speaker].flatten.include?(new_name)
+        next if new_name == speaker
+        (@connections[speaker][degree] << new_name).flatten!
+        @connections[speaker][degree].sort!
+      end
+    end
+    if @connections[speaker][degree].empty?
+      @connections[speaker].pop
+      return
+    else
+      find_mutual_connections_for(speaker, degree + 1)
+    end
   end
 
   def parse(str)
@@ -37,67 +69,115 @@ class DegSep
       scanner.scan_until(/@/)
       names << scanner.scan_until(/\W/).chop
     end
-    @connections[speaker] << [] if @connections[speaker].empty?
-    (@connections[speaker][0] += names).sort!
+    if names.any? 
+      if @connections[speaker].empty?
+        @connections[speaker][0] = []
+      end
+      (@connections[speaker][0] += names).sort!
+    end
+  end
 
+  def to_file(file)
+    str = ""
+    @connections.keys.sort.each do |speaker|
+      names_array = @connections[speaker]
+      str << "#{speaker}\n"
+      names_array.each do |names|
+        str << "#{names.join(', ')}\n"
+      end
+      str << "\n"
+    end
+    str.chomp!
+    File.open(file, 'w') do |f|
+      f.write str
+    end
   end
 end
 
 if $0 == __FILE__
-  raise 'omg, not ready!!'
+  d = DegSep.new
+  d.run("complex_input.txt", 'complex_output.txt')
 else
-  #describe DegSep, '#find_mutual_connections_for' do
-    #before do
-      #@d = DegSep.new
-      #@d.parse_from_file('sample_input.txt')
-    #end
-
-    #it 'works' do
-      #alberta_conns = @d.find_mutual_connections_for('alberta')
-      #expect(alberta_conns).to match_array [
-        #['bob','christie'],['duncan','emily'],['farid']
-      #]
-    #end
-  #end
-
-  describe DegSep, '#parse_from_file' do
+  describe DegSep do
     before do
       @d = DegSep.new
-      @d.parse_from_file('sample_input.txt')
+      @d.parse_from_file('custom_input.txt')
     end
 
-    it 'parses speakers' do
-      expect(@d.connections.keys.sort).to match_array [
-        'alberta', 'bob', 'christie', 'duncan', 'emily', 'farid'
-      ]
+    describe 'run' do
+      it 'works' do
+        File.unlink('my_output.txt') if File.exist?('my_output.txt')
+        d = DegSep.new
+        d.run('custom_input.txt', 'my_output.txt')
+
+        expect(File.read('my_output.txt')).to eq File.read('custom_output.txt')
+      end
     end
 
-    it 'parses names' do
-      expect(@d.connections['alberta']).to match_array [%w(bob christie)]
-      expect(@d.connections['bob']).to match_array [%w(alberta christie duncan)]
-      expect(@d.connections['christie']).to match_array [%w(alberta bob emily)]
-      expect(@d.connections['duncan']).to match_array [%w(bob emily farid)]
-      expect(@d.connections['emily']).to match_array [%w(christie duncan)]
-      expect(@d.connections['farid']).to match_array [%w(duncan)]
-    end
-  end
+    describe '#to_file' do
+      it 'works' do
+        @d.find_all_connections
+        @d.to_file('my_output.txt')
 
-  describe DegSep, '#parse' do
-    it "works on simple" do
-      d = DegSep.new
-      s = "alberta: @bob \"It is remarkable, the character of the pleasure we derive from the best books.\""
-      d.parse(s)
-
-      expect(d.connections.keys).to match_array ["alberta"]
-      expect(d.connections['alberta'].first).to match_array ['bob']
+        expect(File.read('my_output.txt')).to eq File.read('custom_output.txt')
+      end
     end
 
-    it 'works on more complicated' do
-      d = DegSep.new
-      s = "daniella_hamill: @madelyn, @concepcion_hoppe: Power is in nature the essential measure  of right"
-      d.parse(s)
-      expect(d.connections.keys).to eq ["daniella_hamill"]
-      expect(d.connections['daniella_hamill'].first).to match_array %w(concepcion_hoppe madelyn)
+    describe '#find_all_connections' do
+      it 'works' do
+        @d.find_all_connections
+
+        expect(@d.connections['alberta']).to match_array [
+          ['bob','christie'],['duncan','emily'],['farid']
+        ]
+        expect(@d.connections['bob']).to match_array [
+          ['alberta','christie', 'duncan'],['emily','farid']
+        ]
+      end
+    end
+
+    describe '#find_mutual_connections_for' do
+      it 'works for alberta' do
+        @d.find_mutual_connections_for('alberta')
+        alberta_conns = @d.connections['alberta']
+
+        expect(alberta_conns).to match_array [
+          ['bob','christie'],['duncan','emily'],['farid']
+        ]
+      end
+
+      it 'works for bob' do
+        @d.find_mutual_connections_for('bob')
+        bob_conns = @d.connections['bob']
+
+        expect(bob_conns).to match_array [
+          ['alberta','christie', 'duncan'],['emily','farid']
+        ]
+      end
+
+      it 'works for omg' do
+        @d.find_mutual_connections_for('omg')
+        omg_conns = @d.connections['omg']
+
+        expect(omg_conns).to be_empty
+      end
+    end
+
+    describe '#parse_from_file' do
+      it 'parses speakers' do
+        expect(@d.connections.keys.sort).to match_array [
+          'alberta', 'bob', 'christie', 'duncan', 'emily', 'farid', 'omg'
+        ]
+      end
+
+      it 'parses names' do
+        expect(@d.connections['alberta']).to match_array [%w(bob christie)]
+        expect(@d.connections['bob']).to match_array [%w(alberta christie duncan)]
+        expect(@d.connections['christie']).to match_array [%w(alberta bob emily)]
+        expect(@d.connections['duncan']).to match_array [%w(bob emily farid)]
+        expect(@d.connections['emily']).to match_array [%w(christie duncan)]
+        expect(@d.connections['farid']).to match_array [%w(duncan)]
+      end
     end
   end
 end
