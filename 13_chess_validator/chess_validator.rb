d15dc699ab9d82f832aab4c3328d743b0d2aac6c
ian asaff <ian.asaff@gmail.com>
2014-07-06 14:16:12 -0400

    wip

--- 13_chess_validator/chess_validator.rb
+++ 13_chess_validator/chess_validator.rb
@@ -1,4 +1,25 @@
-class Board
+require 'byebug'
+class ChessValidator
 
+  attr_reader :board
+
+  ROW_MAP = Hash[('1'..'8').to_a.reverse.zip(0..7)]
+  COLUMN_MAP = Hash[('a'..'h').zip (0..7)]
+
+  def initialize(board_from_string)
+    parse_and_set_board_from_string(board_from_string)
+  end
+
+  def piece_at_position(position)
+    column, row = position.split('')
+    @board[ROW_MAP[row]][COLUMN_MAP[column]]
+  end
+
+  private
+  def parse_and_set_board_from_string(string)
+    @board = string.split("\n").map do |row|
+      row.split(' ')
+    end
+  end
 end
 
--- 13_chess_validator/chess_validator_spec.rb
+++ 13_chess_validator/chess_validator_spec.rb
@@ -1,7 +1,33 @@
 require_relative 'chess_validator'
 
-describe Board do
-  it 'does something' do
-    expect(Board.new).to_not be_nil
+describe ChessValidator do
+
+  before do
+    @cv = ChessValidator.new(File.read('simple_board.txt'))
   end
+
+  describe '#new' do
+    it 'sets the board correctly' do
+      expect(@cv.board).to match_array [
+        %w(bR bN bB bQ bK bB bN bR),
+        %w(bP bP bP bP bP bP bP bP),
+        %w(-- -- -- -- -- -- -- --),
+        %w(-- -- -- -- -- -- -- --),
+        %w(-- -- -- -- -- -- -- --),
+        %w(-- -- -- -- -- -- -- --),
+        %w(wP wP wP wP wP wP wP wP),
+        %w(wR wN wB wQ wK wB wN wR)
+      ]
+    end
+  end
+
+  describe '#piece_at_position' do
+    it 'returns the correct piece at the given position' do
+      expect(@cv.piece_at_position 'a2').to eq 'wP'
+      expect(@cv.piece_at_position 'e4').to eq '--'
+      expect(@cv.piece_at_position 'g8').to eq 'bN'
+    end
+  end
+
+
 end
