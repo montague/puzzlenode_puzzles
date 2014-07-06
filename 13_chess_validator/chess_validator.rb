22945724cc4b18c66603d07a3837f273170b1fea
ian asaff <ian.asaff@gmail.com>
2014-07-06 18:14:39 -0400

    wip

--- 13_chess_validator/chess_validator.rb
+++ 13_chess_validator/chess_validator.rb
@@ -1,25 +1,79 @@
 require 'byebug'
 class ChessValidator
 
-  attr_reader :board
+  attr_reader :board, :moves
 
   ROW_MAP = Hash[('1'..'8').to_a.reverse.zip(0..7)]
   COLUMN_MAP = Hash[('a'..'h').zip (0..7)]
 
+  LEGAL = 'LEGAL'
+  ILLEGAL = 'ILLEGAL'
+  EMPTY = '--'
+  WHITE = 'w'
+  BLACK = 'b'
+
   def initialize(board_from_string)
-    parse_and_set_board_from_string(board_from_string)
+    @board = parse_array_from_string(board_from_string)
   end
 
-  def piece_at_position(position)
-    column, row = position.split('')
-    @board[ROW_MAP[row]][COLUMN_MAP[column]]
+  def parse_and_set_moves_from_string(string)
+    @moves = parse_array_from_string(string)
+  end
+
+  def at_position(position)
+    row, column = translate_position(position)
+    @board[row][column]
+  end
+
+  def validate_move(from, to)
+    color, piece = at_position(from).split('')
+    case piece
+    when 'P' then validate_pawn_move(from,to,color)
+    when 'R' then 'ROOK'
+    when 'N' then 'KNIGHT'
+    when 'B' then 'BISHOP'
+    when 'Q' then 'QUEEN'
+    when 'K' then 'KING'
+    else
+      raise "WTF??"
+    end
   end
 
   private
-  def parse_and_set_board_from_string(string)
-    @board = string.split("\n").map do |row|
+  def translate_position(position)
+    column, row = position.split('')
+    [ROW_MAP[row].to_i, COLUMN_MAP[column].to_i]
+  end
+
+  def parse_array_from_string(string)
+    string.split("\n").map do |row|
       row.split(' ')
     end
+  end
+
+  def validate_pawn_move(from, to, color)
+    from_column, from_row = from.split('')
+    to_column, to_row = to.split('')
+    # assumption:
+    # black pawns start in row 7
+    # white pawns start in rows 2
+
+    # capturing
+    #if from_column != to_column &&
+      #at_position(to) != EMPTY
+      #if color == WHITE && from_
+
+    #end
+
+    #if from_column == to_column
+      #if color == 'b' && from_row == '7'
+        #return LEGAL if %w(6 5).include?(to_row)
+      #end
+
+      #if color == 'w' && from_row == '2'
+        #return LEGAL if %w(3 4).include?(to_row)
+      #end
+    #end
   end
 end
 
--- 13_chess_validator/chess_validator_spec.rb
+++ 13_chess_validator/chess_validator_spec.rb
@@ -2,32 +2,95 @@ require_relative 'chess_validator'
 
 describe ChessValidator do
 
-  before do
-    @cv = ChessValidator.new(File.read('simple_board.txt'))
-  end
+  context 'basic board stuff' do
 
-  describe '#new' do
-    it 'sets the board correctly' do
-      expect(@cv.board).to match_array [
-        %w(bR bN bB bQ bK bB bN bR),
-        %w(bP bP bP bP bP bP bP bP),
-        %w(-- -- -- -- -- -- -- --),
-        %w(-- -- -- -- -- -- -- --),
-        %w(-- -- -- -- -- -- -- --),
-        %w(-- -- -- -- -- -- -- --),
-        %w(wP wP wP wP wP wP wP wP),
-        %w(wR wN wB wQ wK wB wN wR)
-      ]
+    before do
+      @cv = ChessValidator.new(File.read 'simple_board.txt')
+      @cv.parse_and_set_moves_from_string(File.read 'simple_moves.txt')
+    end
+
+    describe '#new' do
+      it 'sets the board correctly' do
+        expect(@cv.board).to match_array [
+          %w(bR bN bB bQ bK bB bN bR),
+          %w(bP bP bP bP bP bP bP bP),
+          %w(-- -- -- -- -- -- -- --),
+          %w(-- -- -- -- -- -- -- --),
+          %w(-- -- -- -- -- -- -- --),
+          %w(-- -- -- -- -- -- -- --),
+          %w(wP wP wP wP wP wP wP wP),
+          %w(wR wN wB wQ wK wB wN wR)
+        ]
+      end
+    end
+
+    describe '#at_position' do
+      it 'returns the correct piece at the given position' do
+        expect(@cv.at_position 'a2').to eq 'wP'
+        expect(@cv.at_position 'e4').to eq '--'
+        expect(@cv.at_position 'g8').to eq 'bN'
+      end
+    end
+
+    describe '#parse_and_set_moves' do
+      it 'populates a list of moves' do
+        expect(@cv.moves).to match_array [
+          %w(a2 a3),
+          %w(a2 a4),
+          %w(a2 a5),
+          %w(a7 a6),
+          %w(a7 a5),
+          %w(a7 a4),
+          %w(a7 b6),
+          %w(b8 a6),
+          %w(b8 c6),
+          %w(b8 d7),
+          %w(e2 e3),
+          %w(e3 e2)
+        ]
+      end
     end
   end
 
-  describe '#piece_at_position' do
-    it 'returns the correct piece at the given position' do
-      expect(@cv.piece_at_position 'a2').to eq 'wP'
-      expect(@cv.piece_at_position 'e4').to eq '--'
-      expect(@cv.piece_at_position 'g8').to eq 'bN'
+  describe '#validate_move' do
+    before do 
+      @cv = ChessValidator.new(File.read 'complex_board.txt')
+      #   a  b  c  d  e  f  g  h
+      # 8 bK -- -- -- -- bB -- --
+      # 7 -- -- -- -- -- bP -- --
+      # 6 -- bP wR -- wB -- bN --
+      # 5 wN -- bP bR -- -- -- wP
+      # 4 -- -- -- -- wK wQ -- wP
+      # 3 wR -- bB wN wP -- -- --
+      # 2 -- wP bQ -- -- wP -- --
+      # 1 -- -- -- -- -- wB -- --
     end
-  end
 
+    it 'validates a pawn move correctly', focus: true do
+      # one space forward
+      expect(@cv.validate_move 'b2','b3').to eq 'LEGAL'
+      # two spaces forward as first move
+      expect(@cv.validate_move 'b2','b4').to eq 'LEGAL'
+      # capture
+      expect(@cv.validate_move 'b2','c3').to eq 'LEGAL'
+
+      # two spaces forwad after first move
+      expect(@cv.validate_move 'b6','b4').to eq 'ILLEGAL'
+      # one space backward
+      expect(@cv.validate_move 'b6','b7').to eq 'ILLEGAL'
+      # one space forward to occupied space
+      expect(@cv.validate_move 'h5','h4').to eq 'ILLEGAL'
+    end
+
+    it 'validates a rook move correctly'
+
+    it 'validates a knight move correctly'
+
+    it 'validates a bishop move correctly'
+
+    it 'validates a queen move correctly'
+
+    it 'validates a king move correctly'
+  end
 
 end
